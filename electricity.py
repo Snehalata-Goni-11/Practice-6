72eda519aa9b15a90e498ca70aae3b11509cfdb3
Snehalata-Goni-11 <snehagoni60@gmail.com>
2025-10-23 14:03:59 +0530

    save

--- electricity.py
+++ electricity.py
@@ -4,3 +4,12 @@ total_bill = units * rate_per_unit
 
 print(f"Units Consumed: {units}")
 print(f"Total Bill:₹{total_bill}")
+
+if total_bill > 1000:
+    discount = total_bill * 0.10
+    final_bill = total_bill - discount
+    print(f"Discount Applied: ₹{discount}")
+    print(f"Final Bill: ₹{final_bill}")
+else:
+    print("No discount applied.")
+    print(f"Final Bill:₹{total_bill}")
