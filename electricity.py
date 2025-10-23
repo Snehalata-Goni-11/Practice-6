3a71e88a9d7a90d757a002fbe53caa48b8cd52a8
Snehalata-Goni-11 <snehagoni60@gmail.com>
2025-10-23 13:59:02 +0530

    save

---
+++ electricity.py
@@ -0,0 +1,6 @@
+units = int(input("Enter the number of units consumed: "))
+rate_per_unit = 5
+total_bill = units * rate_per_unit
+
+print(f"Units Consumed: {units}")
+print(f"Total Bill:₹{total_bill}")
